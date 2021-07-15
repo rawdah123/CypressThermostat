93bceacad292b1175b2a79ee9e2989bf298a574d
Colin <cln.gardiner@outlook.com>
2021-07-15 09:56:21 +0100

    added commented jQuery in interface.js

--- interface.js
+++ interface.js
@@ -1,12 +1,8 @@
 window.addEventListener("load", function () {
   const thermostat = new Thermostat();
 
-
   // add a <button> element to your html with id of 'button'
   const button = document.getElementById("button");
-
-
-
 
   // add a <span> element to your html with id of 'message'
   const message = document.getElementById("message");
@@ -20,6 +16,15 @@ window.addEventListener("load", function () {
   const usage = document.getElementById("usage");
   power.innerHTML = thermostat.onPowerSaving();
 
+  //JQUERY
+  //jQuery
+  // $("#button").on('click', function() {
+  //   message.innerText = thermostat.up();
+  // });
+  // //jQuery
+  // $("#button-decrease").on('click', function() {
+  //   message.innerText = thermostat.down();
+  // });  
 
 //event listeners go here
   button.addEventListener("click", function () {
